e73b29726a085ae98c35189305fc1cf9403d619c
TobiasAichinger <aichinger.tobias13@gmail.com>
2022-09-15 21:16:31 +0200

    changed error back to main

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ use std::io::{self, Read, BufReader, BufRead};
 use std::fs::File;
 
 mod scanner;
-mod error;
 mod token;
+
+use scanner::Scanner;
+
 
 pub fn main() {
     let args = std::env::args().collect::<Vec<String>>();
@@ -20,21 +22,50 @@ pub fn main() {
 }
 
 fn run_file<T: AsRef<Path>>(path: T) -> io::Result<()> {
-    let file = File::open(path)?;
-    let mut reader = BufReader::new(file);
-    let mut buf: Vec<u8> = vec![];
+    let source: &String = &std::fs::read_to_string(path)?;
 
-    reader.read_to_end(&mut buf)?;
-
-    run(&buf);
+    run(source.as_str());
     Ok(())
 }
 
 fn run_prompt() {
     let stdin = io::stdin();
-    
+    print!("> ");
+
+    for line in stdin.lock().lines() {
+        if let Ok(line) = line {
+            if line.is_empty() {
+                break;
+            }
+
+            run(&line)
+        } else {
+            break;
+        }
+    }
 }
 
-fn run(source: &[u8]) {
+fn run(source: &str) {
+    let scanner: Scanner = Scanner::new(*source);
+    let tokens = scanner.scanTokens();
 
+    for token in tokens {
+        println!("{:?}", token);
+    }
+}
+
+struct Brainfuck {
+    had_error: bool,
+}
+
+impl Brainfuck {
+    pub fn error(&self, line: i32, message: &String) {
+        self.report(line, &"".to_string(), message)
+    }
+    
+    pub fn report(&mut self, line: i32, location: &String, message: &String) {
+        let report: String = format!("[line {line}] Error {location}: {message}");
+        println!("{report}");
+        self.had_error = true;
+    }
 }
