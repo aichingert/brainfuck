89d55b2ad9f606e7de9b6e23cca96cafe2fd3a85
aichingert <aichinger.tobias13@gmail.com>
2023-09-26 12:27:27 +0200

    feat: added basic lexer

--- codegen/src/ast/lexer.rs
+++ codegen/src/ast/lexer.rs
@@ -6,6 +6,14 @@ pub struct Lexer {
 #[derive(Debug)]
 pub enum Token {
     Ident(String),
+    RParen,
+    LParen,
+
+    Declare,
+    Assign,
+    
+    If,
+    Else,
     Invalid,
 }
 
@@ -15,14 +23,40 @@ impl Lexer {
     }
 
     pub fn get_token(&mut self) -> Option<Token> {
-        self.pointer += 1;
+        while self.pointer < self.buffer.len() && self.buffer[self.pointer] == ' ' {
+            self.pointer += 1;
+        }
+
         if self.pointer >= self.buffer.len() {
             return None;
         }
 
-        match self.buffer[self.pointer - 1] {
-            'A' => Some(Token::Ident(String::from("Hello"))),
-            _ => Some(Token::Invalid),
+        let token = match self.buffer[self.pointer] {
+            '(' => Token::LParen,
+            ')' => Token::RParen,
+            ':' => Token::Declare,
+            'a'..='z' | 'A'..='Z' => self.read_identifier(),
+            _ => Token::Invalid,
+        };
+        self.pointer += 1;
+
+        Some(token)
+    }
+
+    fn read_identifier(&mut self) -> Token {
+        let mut acc = String::new();
+        let mut cur: char = self.buffer[self.pointer];
+
+        while self.pointer < self.buffer.len() && cur != ' ' && cur != '\n' {
+            acc.push(cur);
+            self.pointer += 1;
+            cur = self.buffer[self.pointer];
+        }
+
+        match acc.as_str() {
+            "if" => Token::If,
+            "else" => Token::Else,
+            _ => Token::Ident(acc)
         }
     }
 }
--- codegen/src/main.rs
+++ codegen/src/main.rs
@@ -1,10 +1,24 @@
 mod ast;
 use ast::Lexer;
 
-fn main() {
-    let mut lexer: Lexer = Lexer::new(vec!['a', 'A', 'b']);
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 2 {
+        println!("Usage: cargo run [file.bs]");
+        std::process::exit(1);
+    }
+
+    let source: Vec<char> = std::fs::read_to_string(&args[1])?
+        .trim()
+        .to_lowercase()
+        .chars().collect::<Vec<char>>();
+
+    let mut lexer: Lexer = Lexer::new(source);
 
     while let Some(token) = lexer.get_token() {
         println!("{:?}", token);
     }
+
+    Ok(())
 }
